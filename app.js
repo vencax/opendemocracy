2b7cf572b6f39645e995a723b4f576ace561c031
Vaclav Klecanda <vencax77@gmail.com>
2017-04-22 18:40:42 +0200

    adaptation to new kalamata, refactoring

--- app.js
+++ app.js
@@ -22,17 +22,20 @@ app.use(expressJwt(jwtOpts))  //
 app.use(bodyParser.json())  // JSON body parser for parsing incoming data
 
 // setup api
+function _createError(message, status) {
+  return Error({status, message})
+}
 let api = express()
-require('./lib/proposals')(api, db.models.Proposal)
+require('./lib/proposals')(api, db.models.Proposal, _createError)
 app.use('/proposals', api)
 
 api = express()
-require('./lib/comments')(api, db.models.Comment)
+require('./lib/comments')(api, db.models.Comment, _createError)
 app.use('/comments', api)
 
 
 function _general_error_handler(err, req, res, next) {
-  res.status(400).send(err)
+  res.status(err.status || 400).send(err.message || err)
   if (process.env.NODE_ENV !== 'production') {
     console.log('---------------------------------------------------------')
     console.log(err)
