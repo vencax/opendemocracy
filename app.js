206a67d29c285c5c7a8b5a4612f4056e7ae4834f
vencax <vencax@centrum.cz>
2017-12-26 11:21:19 +0100

    sendmail app change

--- app.js
+++ app.js
@@ -1,59 +1,50 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const expressJwt = require('express-jwt')
 const db = require('./db')
 const fakeusers = require('./fakeusers')
 
-// create app
-const app = express()
+module.exports = (app, sendMail) => {
+  // JSON body parser for parsing incoming data
+  app.use(bodyParser.json())
 
-// init CORS
-const opts = {
-  maxAge: 86400,
-  origin: process.env.ALLOWED_ORIGIN || '*',
-  methods: ['DELETE', 'PUT', 'POST', 'OPTIONS', 'GET'],
-  exposedHeaders: ['x-total-count']
-}
-app.use(cors(opts))
-app.use(bodyParser.json())  // JSON body parser for parsing incoming data
-
-// setup api
-function _createError (message, status) {
-  return {status: status || 400, message}
-}
-
-const g = {
-  authMW: expressJwt({secret: process.env.SERVER_SECRET}),
-  createError: _createError,
-  models: db.models,
-  startTransaction: db.startTransaction
-}
-
-fakeusers(app, g)  // init auth routes
-
-const manager = require('./lib/manager')(db)
-
-let api = express()
-require('./lib/proposals')(api, g, manager)
-app.use('/proposals', api)
-
-api = express()
-require('./lib/comments')(api, g)
-app.use('/comments', api)
-
-api = express()
-require('./lib/notifications').initApi(api, g)
-app.use('/notifications', api)
-
-function _generalErrorHandler (err, req, res, next) {
-  res.status(err.status || 400).send(err.message || err)
-  if (process.env.NODE_ENV !== 'production') {
-    console.log('---------------------------------------------------------')
-    console.log(err)
-    console.log('---------------------------------------------------------')
+  // setup api
+  function _createError (message, status) {
+    return {status: status || 400, message}
   }
-}
-app.use(_generalErrorHandler)
 
-module.exports = app
+  const g = {
+    authMW: expressJwt({secret: process.env.SERVER_SECRET}),
+    createError: _createError,
+    models: db.models,
+    startTransaction: db.startTransaction
+  }
+
+  fakeusers(app, g)  // init auth routes
+
+  const mailer = require('./lib/mailer')(sendMail)
+  const manager = require('./lib/manager')(db, mailer)
+
+  let api = express()
+  require('./lib/proposals')(api, g, manager)
+  app.use('/proposals', api)
+
+  api = express()
+  require('./lib/comments')(api, g)
+  app.use('/comments', api)
+
+  api = express()
+  require('./lib/notifications').initApi(api, g)
+  app.use('/notifications', api)
+
+  function _generalErrorHandler (err, req, res, next) {
+    res.status(err.status || 400).send(err.message || err)
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('---------------------------------------------------------')
+      console.log(err)
+      console.log('---------------------------------------------------------')
+    }
+  }
+  app.use(_generalErrorHandler)
+  return app
+}
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 const port = process.env.PORT || 3000
-const app = require('./app')
+const App = require('./app')
 const db = require('./db')
+
+function sendMail (mail) {
+  return new Promise((resolve, reject) => {
+    console.log(mail)
+    resolve(mail)
+  })
+}
+
+const app = express()
+
+// init CORS?
+const opts = {
+  maxAge: 86400,
+  origin: process.env.ALLOWED_ORIGIN || '*',
+  methods: ['DELETE', 'PUT', 'POST', 'OPTIONS', 'GET'],
+  exposedHeaders: ['x-total-count']
+}
+app.use(cors(opts))
 
 db.migrate.latest()
 .then(() => {
-  app.listen(port, function () {
+  App(app, sendMail).listen(port, function () {
     console.log('gandalf do magic on ' + port)
   })
 })
